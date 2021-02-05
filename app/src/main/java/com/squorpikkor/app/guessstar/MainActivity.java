3e073d6395fca86e4b3a3b6b3eb474a48ded656e
VadimSquorpikkor <uniqueehub1024>
2021-02-05 22:18:47 +0300

    Initial commit

---
+++ app/src/main/java/com/squorpikkor/app/guessstar/MainActivity.java
@@ -0,0 +1,203 @@
+package com.squorpikkor.app.guessstar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.service.autofill.FieldClassification;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ExpandableListView;
+import android.widget.ImageView;
+import android.widget.QuickContactBadge;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.concurrent.ExecutionException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MyTag";
+    Button button0;
+    Button button1;
+    Button button2;
+    Button button3;
+    ImageView imageView;
+
+    ArrayList<String> urls;
+    ArrayList<String> names;
+    ArrayList<Button> buttons;
+
+    private int numberOfQuestion;
+    private int numberOfRightAnswer;
+
+    String site = "https://www.forbes.ru/rating/403469-40-samyh-uspeshnyh-zvezd-rossii-do-40-let-reyting-forbes";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button0 = findViewById(R.id.button0);
+        button1 = findViewById(R.id.button1);
+        button2 = findViewById(R.id.button2);
+        button3 = findViewById(R.id.button3);
+        imageView = findViewById(R.id.image_view);
+
+
+        urls = new ArrayList<>();
+        names = new ArrayList<>();
+        buttons = new ArrayList<>();
+        buttons.add(button0);
+        buttons.add(button1);
+        buttons.add(button2);
+        buttons.add(button3);
+
+        getContent();
+
+        playGame();
+
+        button0.setOnClickListener(view -> onClickAnswer(0));
+        button1.setOnClickListener(view -> onClickAnswer(1));
+        button2.setOnClickListener(view -> onClickAnswer(2));
+        button3.setOnClickListener(view -> onClickAnswer(3));
+    }
+
+    void onClickAnswer(int q) {
+        if (q == numberOfRightAnswer) Toast.makeText(this, "Верно!", Toast.LENGTH_SHORT).show();
+        else Toast.makeText(this, "Неверно! Правильный ответ — " + names.get(numberOfQuestion), Toast.LENGTH_SHORT).show();
+        playGame();
+    }
+
+    private void getContent() {
+        DownloadContentTask task = new DownloadContentTask();
+        try {
+            String content = task.execute(site).get();
+            String start = "<tbody>";
+            String finish = "</tbody>";
+            Pattern pattern = Pattern.compile(start + "(.*?)" + finish);
+            Matcher matcher = pattern.matcher(content);
+            String splitContent = "";
+            while (matcher.find()) {
+                splitContent = matcher.group(1);
+            }
+            Pattern patternImage = Pattern.compile("<img src=\"(.*?)\"");
+            Pattern patternNames = Pattern.compile("alt=\"(.*?)\"");
+            Matcher matcherImage = patternImage.matcher(splitContent);
+            Matcher matcherNames = patternNames.matcher(splitContent);
+            while (matcherImage.find()) {
+                urls.add(matcherImage.group(1));
+            }
+            while (matcherNames.find()) {
+                names.add(matcherNames.group(1));
+            }
+            for (String s:names) {
+                Log.e(TAG, s);
+            }
+            Log.e("TAG", splitContent);
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void playGame() {
+        generateQuestion();
+        DownloadImageTask task = new DownloadImageTask();
+        try {
+            Bitmap bitmap = task.execute(urls.get(numberOfQuestion)).get();
+            if (bitmap != null) {
+                imageView.setImageBitmap(bitmap);
+                for (int i = 0; i < buttons.size(); i++) {
+                    if (i == numberOfRightAnswer) {
+                        buttons.get(i).setText(names.get(numberOfQuestion));
+                    } else {
+                        buttons.get(i).setText(names.get(generateWrongQuestion()));
+                    }
+                }
+            }
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void generateQuestion() {
+        numberOfQuestion = (int) Math.random() * names.size();
+        numberOfRightAnswer = (int) Math.random() * buttons.size();
+    }
+
+    private int generateWrongQuestion() {
+        return (int)Math.random()*names.size();
+    }
+
+    private static class DownloadContentTask extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... strings) {
+            URL url = null;
+            HttpURLConnection urlConnection = null;
+            StringBuilder result = new StringBuilder();
+            try {
+                url = new URL(strings[0]);
+                urlConnection = (HttpURLConnection)url.openConnection();
+                InputStream in = urlConnection.getInputStream();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+                String line = reader.readLine();
+                while (line != null) {
+                    result.append(line);
+                    line = reader.readLine();
+                }
+                return result.toString();
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+            }
+            return null;
+        }
+    }
+
+    private static class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
+
+        @Override
+        protected Bitmap doInBackground(String... strings) {
+            URL url = null;
+            HttpURLConnection urlConnection = null;
+            try {
+                url = new URL(strings[0]);
+                urlConnection = (HttpURLConnection)url.openConnection();
+                InputStream in = urlConnection.getInputStream();
+                Bitmap bitmap = BitmapFactory.decodeStream(in);
+                return bitmap;
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+            }
+            return null;
+        }
+    }
+}
